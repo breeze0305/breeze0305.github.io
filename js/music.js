839158c75295a32435674a810093b5d41be733f0
breeze0305 <benfeng99@gmail.com>
2023-03-20 20:04:07 +0800

    Site updated: 2023-03-20 20:04:07

--- js/music.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var audio = new Audio();
-    audio.src = '/music/music.mp3'; // 更改文件名稱
-    audio.loop = true;
-    audio.play();
-  });
